829977bde148429f76aacb03447dbee85a6b8312
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-09-18 10:16:15 +0300

    images added, complete version of presentation

--- routes/chapter6.js
+++ routes/chapter6.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('chapter6', { title: 'Thanks' });
+  res.render('chapter6', { title: 'Influence on  markup' });
 });
 
 module.exports = router;
